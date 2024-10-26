5427d3a819a9a519536a8213b85638d6efa70835
ayush-verma1708 <ayush.verma@s.amity.edu>
2024-10-26 17:14:04 +0530

    Complete Base

--- src/components/Stats.jsx
+++ src/components/Stats.jsx
@@ -5,32 +5,32 @@ import StatItem from './StatItem';
 const Stats = () => {
   const [stats] = useState([
     {
-      statIcon: "fa-solid fa-users",
-      statTitle: "Total Customers",
-      statValue: "5,423",
+      statIcon: 'fa-solid fa-users',
+      statTitle: 'Total Customers',
+      statValue: '5,423',
     },
     {
-      statIcon: "fa-solid fa-user-check",
-      statTitle: "Admins",
-      statValue: "3",
+      statIcon: 'fa-solid fa-user-check',
+      statTitle: 'Admins',
+      statValue: '3',
     },
     {
-      statIcon: "fa-solid fa-book-open",
-      statTitle: "Total Magazines",
-      statValue: "189",
+      statIcon: 'fa-solid fa-book-open',
+      statTitle: 'Total Magazines',
+      statValue: '189',
     },
   ]);
 
   return (
-    <div className="stats-container">
+    <div className='stats-container'>
       {stats.map((stat, index) => (
         <React.Fragment key={index}>
-          <StatItem 
-            statIcon={stat.statIcon} 
-            statTitle={stat.statTitle} 
-            statValue={stat.statValue} 
+          <StatItem
+            statIcon={stat.statIcon}
+            statTitle={stat.statTitle}
+            statValue={stat.statValue}
           />
-          {index < stats.length - 1 && <hr className="vertical-hr" />}
+          {index < stats.length - 1 && <hr className='vertical-hr' />}
         </React.Fragment>
       ))}
     </div>
@@ -38,4 +38,3 @@ const Stats = () => {
 };
 
 export default Stats;
-
